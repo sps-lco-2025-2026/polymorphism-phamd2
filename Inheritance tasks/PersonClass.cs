5eb2606cd8316dd5ec7add8de93842634c3e1f43
phamd2 <phamd@stpaulsschool.org.uk>
2026-04-28 09:43:08 +0100

    Classes Updated

--- Inheritance tasks/PersonClass.cs
+++ Inheritance tasks/PersonClass.cs
@@ -1,6 +1,105 @@
-﻿namespace Inheritance_tasks;
+﻿using System.Dynamic;
 
-public class Class1
+
+namespace Inheritance_Tasks;
+
+
+public class Person
 {
+   private string _firstName ;
+   private string _lastName;
+   private string _emailAddress;
+   private string _dateofBirth;
 
+
+  
+
+
+   public Person(string firstname, string lastname, string dob, string email)
+   {
+      _firstName = firstname;
+      _lastName = lastname;
+      _dateofBirth = dob;
+      _emailAddress = email;
+   }
+
+
+   public Person(string firstname, string lastname, string dob)
+   {
+      _firstName = firstname;
+      _lastName = lastname;
+      _dateofBirth = dob;
+     
+   }
+
+
+   internal int CalculateAge()
+   {
+       DateTime dobDateTime = DateTime.Parse(_dateofBirth);
+       int age = DateTime.Now.Year - dobDateTime.Year;
+       if (dobDateTime > DateTime.Now.AddYears(-age)) 
+        age--;
+       return age;
+   }
+
+
+   internal bool checkAge()
+   {
+       if (CalculateAge()<=0)
+           return false;
+       else
+           return true;
+   }
+
+
+   public bool checkAdult()
+   {  
+       DateTime dobDateTime;
+       return DateTime.TryParse(_dateofBirth, out dobDateTime);
+
+
+   }
+
+
+   public string ChineseSign()
+   {
+           DateTime dob = DateTime.Parse(_dateofBirth);
+           int year = dob.Year;
+          
+           string[] signs = {
+               "Rat", "Ox", "Tiger", "Rabbit", "Dragon", "Snake",
+               "Horse", "Goat", "Monkey", "Rooster", "Dog", "Pig"
+           };
+
+
+           int offset = (year - 1900) % 12;
+           return signs[offset];
+   }
+
+
+   public bool IsBirthday()
+   {
+           DateTime dob = DateTime.Parse(_dateofBirth);
+           return dob.Month == DateTime.Today.Month && dob.Day == DateTime.Today.Day;
+   }
+
+
+   public string screenName()
+   {
+       DateTime dob = DateTime.Parse(_dateofBirth);
+       string firstInitial = _firstName.Substring(0, 1).ToLower();
+       string lastName = _lastName.Replace(" ", "").ToLower();
+
+
+       string username = $"{firstInitial}{lastName}{dob.Month}{dob.Day}";
+       return username;
+      
+   }
 }
+
+
+
+
+
+  
+
--- Inheritance tasks/StudentClass.cs
+++ Inheritance tasks/StudentClass.cs
@@ -1,17 +1,37 @@
-﻿using System.Dynamic;
-
-
-namespace InheritanceTasks;
-
+﻿namespace Inheritance_Tasks;
 
 public class Student : Person
 {
    public string SchoolYear;
-  
-  
-   public Student(string firstname, string lastname, string dob, string email);
+   public string yearGroup;
+
+
+
+   public Student(string firstname, string lastname, string dob, string email, string schoolYear, string yearGroup) : base(firstname, lastname, dob, email)
+   {
+      schoolYear = CalculateSchoolYear(dob);
+      yearGroup = CalculateEstimatedYear();
+
+   }
+
+   private string CalculateSchoolYear(string dob)
+   {
+      DateTime dobDateTime = DateTime.Parse(dob);
+      return $"{DateTime.Now.Year - dobDateTime.Year}";
+   }
+
+   private string CalculateEstimatedYear()
+   {
+      int age = CalculateAge();
+
+      if (age >= 5 && age <=17)
+         return $"Year {age-4}";
+      else if (age == 4)
+         return "Reception";
+      else
+         return "Invalid Year";
+   }
+    
+   
 }
-
-
-  
 
